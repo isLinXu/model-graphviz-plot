25d3c0ee6c87d2a51d94efcce4fc3e7b2dc14c97
isLinXu <2267379130@qq.com>
2023-11-02 00:18:47 +0800

    [mq_plot][utils] add hf_info_print.py

---
+++ mq_plot/utils/hf_info_print.py
@@ -0,0 +1,24 @@
+
+import torch
+from transformers import AutoTokenizer, AutoModel
+from torchinfo import summary
+
+
+def print_model_summary(model_name_or_path):
+    # Load the tokenizer and model dynamically based on the model name or path
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    model = AutoModel.from_pretrained(model_name_or_path)
+
+    # Create a sample input
+    input_text = "Hello, how are you today?"
+    input_ids = torch.tensor(tokenizer.encode(input_text, add_special_tokens=True)).unsqueeze(0)
+
+    # Print the model summary using torchinfo
+    print(f"Model: {model_name_or_path}")
+    print(f"Input Text: {input_text}")
+    summary(model, input_data=input_ids)
+
+
+# Example usage:
+print_model_summary('gpt2')
+print_model_summary('bert-base-uncased')
